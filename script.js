6250efaef68ec05675fb019fe2c8b35550e00dbe
anonimo504a <136383187+anonimo504a@users.noreply.github.com>
2025-05-24 17:38:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-        alternativas: [
-            {
-                texto: "Isso é assustador!",
-                afirmacao: "afirmação"
-            },
-            {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmação"
-            }
-        ]
+       enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia...",
+alternativas: [
+  {
+    texto: "Isso é assustador!",
+    afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
+  },
+  {
+    texto: "Isso é maravilhoso!",
+    afirmacao: "Quis saber como usar IA no seu dia a dia."
+  }
+]
     },
     {
         enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
